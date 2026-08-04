2d5aedc5c442a27c8dcf9a7580e1ed605accb1d9
nader elfeel <naderelfeel@gmail.com>
2026-08-05 02:54:49 +0300

    added save and load catalog(fully persistent on disk)

--- src/Catalog/Catalog.c++
+++ src/Catalog/Catalog.c++
@@ -56,9 +56,125 @@ vector<TableInfo*> Catalog::GetTables(){
     return res;
 }
 
+void Catalog::save_catalog(){
+    //create a buffer to serialize data into
+    char* buffer = new char[PAGE_SIZE];
+    int index{0};
+    
+    //save first_page_id
+    memcpy(buffer+index, &catalog_first_page_id, sizeof(catalog_first_page_id));
+    index += sizeof(catalog_first_page_id);
+    
+    //save last_page_id
+    memcpy(buffer+index, &catalog_last_page_id, sizeof(catalog_last_page_id));
+    index += sizeof(catalog_last_page_id);
 
+    //save tables size
+    int number_of_tables = tables.size();
+    cout<<number_of_tables<<endl;
+    memcpy(buffer+index, &number_of_tables, sizeof(number_of_tables));
+    index += sizeof(number_of_tables);
+
+    //save actual table 
+    for(auto&[table_name, tableInfo]:this->tables){
+        tableInfo->serializeTableInfo(buffer+index);
+        index+=tableInfo->getSize();
+    }
+
+    //now copy the whole buffer into the first page then save it to the disk
+    char* page_buffer = this->BPM->fetchPage(catalog_first_page_id);
+    memcpy(page_buffer, buffer, PAGE_SIZE);
+    // mark as direty jsut to be presistent on disk
+    BPM->markAsDirty(catalog_first_page_id);
+    cout << "Saving to page: " << catalog_first_page_id << endl;
+    //delete the buffer allocated
+    delete[]buffer;
+
+}
+
+void Catalog::load_catalog(int page_id=1){
+    //load the page 
+    //by default it's the second page in the whole file (page_id =1)
+    char* buffer = this->BPM->fetchPage(page_id);
+    int offset{0};
+    cout << "Loading from page: " << page_id << endl;
+    //load data 
+
+    //load first_page_id
+    memcpy(&this->catalog_first_page_id,buffer+offset, sizeof(catalog_first_page_id));
+    offset += sizeof(catalog_first_page_id);
+
+    //load last_page_id
+    memcpy(&this->catalog_last_page_id,buffer+offset, sizeof(catalog_last_page_id));
+    offset += sizeof(catalog_last_page_id);
+
+    //load tables size
+    int number_of_tables;
+    memcpy(&number_of_tables, buffer+offset, sizeof(number_of_tables));
+    offset += sizeof(number_of_tables);
+
+    cout<<"number of tables : "<<number_of_tables<<endl;
+    tables.clear();
+    //laod actual table 
+    for(int i=0;i<number_of_tables;i++){
+        TableInfo* table_info = new TableInfo();
+        table_info->loadTableInfo(buffer+offset);
+        offset+=table_info->getSize();
+
+        string table_name=  table_info->table_name;
+        tables[table_name] = table_info;
+    }
+
+}
 
 int
 main(){
 
+    DiskManager* dm = new DiskManager("catalog.db");
+    BufferPoolManager* BPM = new BufferPoolManager(dm);
+
+    Catalog* catalog = new Catalog(BPM, true);
+    string table_name = "User";
+
+    Column t1_col1 = Column(TYPE_INT, "user_id", sizeof(int));
+    Column t1_col2 = Column(TYPE_STRING, "firstName", 30);
+    Column t1_col3 = Column(TYPE_STRING, "lastName", 30);
+    Column t1_col4 = Column(TYPE_INT, "age", sizeof(int));
+    vector<Column> user_schema = {t1_col1, t1_col2, t1_col3, t1_col4};
+
+    catalog->CreateTable(table_name, user_schema);
+    catalog->CreateTable("Order", user_schema);
+
+    for(auto&[table_name, table_info]: catalog->getTables()){
+        cout<<table_name<<endl;
+        cout<<table_info->table_name<<endl;
+        for(auto&col: table_info->schema){
+            col.printCol();
+        }
+        cout<<endl;
+    }
+    cout<<"--------------"<<endl;
+
+    catalog->save_catalog();
+    //delete catalog;
+    BPM->~BufferPoolManager();
+    dm->~DiskManager();
+
+
+    DiskManager* dm2 = new DiskManager("catalog.db");
+    BufferPoolManager* BPM2 = new BufferPoolManager(dm2);
+
+    Catalog* catalog2 = new Catalog(BPM2, false);
+    //retest after loading
+    catalog2->load_catalog();
+
+    for(auto&[table_name, table_info]: catalog2->getTables()){
+        cout<<table_name<<endl;
+        cout<<table_info->table_name<<endl;
+        for(auto&col: table_info->schema){
+            col.printCol();
+        }
+        cout<<endl;
+    }
+    cout<<"--------------"<<endl;
 }
--- src/Catalog/Catalog.h
+++ src/Catalog/Catalog.h
@@ -11,53 +11,6 @@
 using namespace std;
 
 
-struct TableInfo
-{
-        int offset{0};
-        
-        //uint32_t table_id;
-        string table_name;
-        vector<Column> schema;
-
-        int first_page_id;
-        TableHeap* table_heap;
-        vector<IndexInfo> indexes;
-
-        void serializeTableInfo(char* buffer){
-            // save table_name length
-            int table_name_size = table_name.length();
-            memcpy(buffer+offset, &table_name_size, sizeof(table_name_size));
-            offset += sizeof(table_name_size);
-
-            //save first page_id of the table
-            memcpy(buffer+offset, &first_page_id, sizeof(first_page_id));
-            offset += sizeof(first_page_id);
-
-            // save table schema
-            for(auto&col:schema){
-                //this function serializes the whole col adn saved into buffer
-                col.serializeCol(buffer+offset);
-                offset += col.getColSize();   
-            }
-
-            // save table indexes:
-            for(auto&index:indexes){
-                index.serializeIndex(buffer+offset);
-                offset += index.getSize();
-            }
-
-        }
-
-        int getSize(){return offset;}
-
-};
-
-
-typedef enum{
-    STATIC_HASH_INDEX,
-    BPLUS_TREE_INDEX
-}indexes_t;
-
 struct IndexInfo
 {
     int offset = {0};
@@ -71,27 +24,208 @@ struct IndexInfo
 
     void serializeIndex(char* buffer){
         //int index{0};
+        offset = 0;
+
         // save index_name len (to be able to load it dynamically)
         int index_name_size = name.length();
         memcpy(buffer+offset, &index_name_size, sizeof(index_name_size));
         offset += sizeof(index_name_size);
  
+        //save index name
+        memcpy(buffer+offset, name.data(), name.size());
+        offset += name.size();
+
         //same with col_name that the index is built on
         int col_name_size = column_name.length();
         memcpy(buffer+offset, &col_name_size, sizeof(col_name_size));
         offset += sizeof(col_name_size);
+
+        //save column name
+        memcpy(buffer+offset, column_name.data(), column_name.size());
+        offset += column_name.size();
+
         // save the type of the index
         memcpy(buffer+offset, &type, sizeof(type));
         offset += sizeof(type);
+
         //save first page_id of the index where so coiuld be loaded later
         memcpy(buffer+offset, &root_page, sizeof(root_page));
         offset += sizeof(root_page);
     }
-    
+
+    void loadIndex(const char *buffer){
+        offset = 0;
+
+        //load index name length
+        int len; 
+        memcpy(&len, buffer +offset, sizeof(len));
+        offset += sizeof(len);
+
+        //load index name
+        name.assign(buffer+offset, len);
+        offset += len;
+
+        //load col name size
+        memcpy(&len, buffer + offset, sizeof(len));
+        offset += sizeof(len);
+
+        // load col name
+        column_name.assign(buffer + offset, len);
+        offset += len;
+
+        //load index type
+        memcpy(&type, buffer + offset, sizeof(type));
+        offset += sizeof(type);
+
+        //load index page_id
+        memcpy(&root_page, buffer + offset, sizeof(root_page));
+        offset += sizeof(root_page);
+    }
+
     int getSize(){return offset;}
     
-    
 };
+
+
+
+struct TableInfo
+{
+        int offset{0};
+        
+        //uint32_t table_id;
+        string table_name;
+        vector<Column> schema;
+
+        int first_page_id;
+        TableHeap* table_heap;
+        vector<IndexInfo> indexes;
+
+        void serializeTableInfo(char* buffer){
+            offset = 0;
+            // save table_name length
+            int table_name_size = table_name.length();
+            memcpy(buffer+offset, &table_name_size, sizeof(table_name_size));
+            offset += sizeof(table_name_size);
+
+            //save table_name
+            memcpy(buffer+offset, table_name.data(), table_name.size());
+            offset += table_name.size();
+
+            //save first page_id of the table
+            memcpy(buffer+offset, &first_page_id, sizeof(first_page_id));
+            offset += sizeof(first_page_id);
+
+            //save number of cols in schema
+            int schema_size = schema.size();
+            memcpy(buffer+offset, &schema_size, sizeof(schema_size));
+            offset += sizeof(schema.size());
+
+            // save each col
+            for(auto& c : this->schema){
+                c.printCol();
+                switch (c.getColType()){
+                case TYPE_INT:
+                    {buffer[offset] = 'I'; break;}
+                case TYPE_STRING:
+                    {buffer[offset] = 'S'; break;}
+                case TYPE_FLOAT:
+                    {buffer[offset] = 'F'; break;}
+                case TYPE_BOOL:
+                    {buffer[offset] = 'B'; break;}
+                default:
+                    cerr<<"incorrect field type"<<endl;
+                    break;
+                }
+                offset+=1;
+                cout<<"from save table meta"<<endl;
+                c.serializeCol(buffer+ offset);
+                offset += c.getColSize();
+            }
+
+            //save number of indexes
+            int indexes_size = indexes.size();
+            memcpy(buffer+offset, &indexes_size, sizeof(indexes_size));
+            offset += sizeof(indexes.size());
+
+            // save table indexes:
+            for(auto&index:indexes){
+                index.serializeIndex(buffer+offset);
+                offset += index.getSize();
+            }
+
+        }
+
+        void loadTableInfo(char *buffer){
+            offset = 0;
+        
+            int len;
+            //load table name len
+            memcpy(&len, buffer + offset, sizeof(len));
+            offset += sizeof(len);
+            //load table name
+            table_name.assign(buffer + offset, len);
+            offset += len;
+        
+            //load the first page id
+            memcpy(&first_page_id, buffer + offset, sizeof(first_page_id));
+            offset += sizeof(first_page_id);
+        
+            //load schema size
+            int schema_size;
+            memcpy(&schema_size, buffer + offset, sizeof(schema_size));
+            offset += sizeof(schema_size);
+        
+            schema.clear();
+            //load every col and add it to the schema
+
+            for(int i=0;i<schema_size;i++){
+                char type = buffer[offset];
+                //cout<<"Type : "<<type<<endl;
+                FieldType field_type;
+                switch (type)
+                {
+                case 'I':
+                    {field_type =  TYPE_INT; break;}
+                case 'S':
+                    {field_type =  TYPE_STRING; break;}
+                case 'F':
+                    {field_type =  TYPE_FLOAT; break;}
+                case 'B':
+                    {field_type =  TYPE_BOOL; break;}
+                default:
+                    cerr<<"invalid field type"<<endl;
+                    break;
+                }
+                offset+=1;
+                Column c(field_type,"",1);
+                c.deSerializeCol(buffer+ offset);
+                //c.getField()->print();
+                //c.printCol();
+                this->schema.push_back(c);
+                offset += c.getColSize();
+            }
+
+            //load index size
+            int indexes_size;
+            memcpy(&indexes_size, buffer + offset, sizeof(indexes_size));
+            offset += sizeof(indexes_size);
+        
+            indexes.clear();
+            // load all indexes
+            for (int i = 0; i < indexes_size; i++){
+                IndexInfo idx;
+                idx.loadIndex(buffer + offset);
+                // add index size then push table indexes
+                offset += idx.getSize();
+                indexes.push_back(idx);
+            }
+        }
+
+        int getSize(){return offset;}
+
+};
+
+
 
 
 class Catalog
@@ -109,8 +243,23 @@ class Catalog
 
 
     public:
-        Catalog(BufferPoolManager* BPM):BPM(BPM){
-            this->catalog_last_page_id = this->BPM->newPage();
+        Catalog(BufferPoolManager* BPM, bool createNew):BPM(BPM){
+            if(createNew){
+                // allocate a page for first and last  page
+                this->catalog_first_page_id = this->BPM->newPage();
+                this->catalog_last_page_id = this->BPM->newPage();
+                cout<<"first page id : "<<catalog_first_page_id<<endl;;
+                cout<<"last page id : "<<catalog_last_page_id<<endl;
+
+                //update next pointer of  first page to the last one
+                char* buffer = this->BPM->fetchPage(catalog_first_page_id);
+                PageHeader* header = reinterpret_cast<PageHeader*>(buffer);
+                header->next_page_id = catalog_last_page_id;
+
+                //mark as dirty just to be saved to disk later
+                this->BPM->markAsDirty(catalog_first_page_id);
+            }
+
         }
 
         TableInfo* CreateTable(string table_name, vector<Column>schema);
@@ -120,25 +269,13 @@ class Catalog
         bool TableExists(string table_name);
         vector<TableInfo*> GetTables();
 
-        void save_catalog(){
-            char* buffer = this->BPM->fetchPage(catalog_first_page_id);
-            int index = sizeof(PageHeader);
-            
-            memcpy(buffer+index, &catalog_first_page_id, sizeof(catalog_first_page_id));
-            index += sizeof(catalog_first_page_id);
+        void save_catalog();
+        void load_catalog(int catalog_first_page_id);
 
-            memcpy(buffer+index, &catalog_last_page_id, sizeof(catalog_last_page_id));
-            index += sizeof(catalog_last_page_id);
-
-            int number_of_tables = tables.size();
-            memcpy(buffer+index, &number_of_tables, sizeof(number_of_tables));
-            index += sizeof(number_of_tables);
-
+        unordered_map<string,TableInfo*> getTables(){
+            return this->tables;
         }
 
-        void load_catalog(){
-
-        }
 
 };
 
