7cc0abf3a63dd337499ccc13157de609d22e7173
nader elfeel <naderelfeel@gmail.com>
2026-08-21 18:40:09 +0300

    bounding the statement to match any query type

---
+++ src/Binder/BoundStatement.h
@@ -0,0 +1,29 @@
+#ifndef BOUND_STATEMENT_H
+#define BOUND_STATEMENT_H
+
+#include"D:\SWE\DB\CMU\MY_DB_ENGINE\Minimal_DB_ENGINE\src\Catalog\Catalog.h"
+#include"D:\SWE\DB\CMU\MY_DB_ENGINE\Minimal_DB_ENGINE\parser\external\sql-parser\src\SQLParser.h"
+#include"D:\SWE\DB\CMU\MY_DB_ENGINE\Minimal_DB_ENGINE\parser\external\sql-parser\src\SQLParserResult.h"
+
+using namespace std;
+
+enum class BoundStatementType {
+    SELECT,
+    INSERT,
+    UPDATE,
+    DELETE,
+    CREATE_TABLE,
+    DROP_TABLE
+};
+
+
+class BoundStatement {
+public:
+
+    virtual ~BoundStatement() = default;
+
+    virtual BoundStatementType
+    type() const = 0;
+};
+
+#endif
